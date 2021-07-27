225223073073cdec629dbb29eb0ecee236802c42
Damla Dalgıç <damlabileydi@gmail.com>
2021-07-27 09:19:53 +0300

    Bireysel giriş ve kayıt ol ekranı eklendi.

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-app>
+    <v-main>
+      <router-view>
+        <app-individual-login />
+      </router-view>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import IndividualLogin from "./components/IndividualLogin.vue";
+
+export default {
+  name: "App",
+
+  components: {
+    appIndividualLogin: IndividualLogin,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
